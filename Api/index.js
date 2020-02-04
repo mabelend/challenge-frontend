bd105b594988a0292df6bf78706cf86f7804b03c
martín abelenda <ext_mabelend@ar0c02w809yhv2j.ml.com>
2020-02-04 15:35:35 -0300

    Agrego prototipo de API con Express y agrego gitignore

---
+++ Api/index.js
@@ -0,0 +1,30 @@
+var app = express();
+var router = express.Router();
+
+router.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  next();
+});
+
+router.use('/user/:id', function(req, res, next) {
+  console.log('Request URL:', req.originalUrl);
+  next();
+}, function (req, res, next) {
+  console.log('Request Type:', req.method);
+  next();
+});
+
+router.get('/user/:id', function (req, res, next) {
+
+    if (req.params.id == 0) next('route');
+  else next(); 
+}, function (req, res, next) {
+  res.render('regular');
+});
+
+router.get('/user/:id', function (req, res, next) {
+  console.log(req.params.id);
+  res.render('special');
+});
+
+app.use('/', router);
